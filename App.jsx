6fc9855eb5bec877a957a8e7d39d97ccb22af15b
Aszellem <74776542+Aszellem@users.noreply.github.com>
2022-02-05 19:13:58 +0100

    nyilt_fuggveny

--- App.jsx
+++ App.jsx
@@ -3,26 +3,23 @@ import Card from "./Cards";
 import contracts from "../contacts";
 import Avatar from "./Avatar";
 
-function CreateCard(contact) {
-  return (
-    <div>
-      <Card
-        key={contact.id}
-        name={contact.name}
-        img={contact.imgURL}
-        tel={contact.phone}
-        email={contact.email}
-      />
-    </div>
-  );
-}
-
 function App() {
   return (
     <div>
       <Avatar img="https://upload.wikimedia.org/wikipedia/commons/thumb/8/88/Danny_DeVito_cropped_and_edited_for_brightness.jpg/514px-Danny_DeVito_cropped_and_edited_for_brightness.jpg" />
       <h1 className="heading">Ismerősök</h1>
-      {contracts.map(CreateCard)}
+
+      {contracts.map(function (contact) {
+        return (
+          <Card
+            key={contact.id}
+            name={contact.name}
+            img={contact.imgURL}
+            tel={contact.phone}
+            email={contact.email}
+          />
+        );
+      })}
     </div>
   );
 }
